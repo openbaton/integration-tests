6f8651523dcab8c2da56add6497792333d0c64cb
tiziartas <cecamoretiziano25@gmail.com>
2015-06-24 15:25:29 +0200

    mysql check

--- src/main/java/org/project/neutrino/integration/test/MainIntegrationTest.java
+++ src/main/java/org/project/neutrino/integration/test/MainIntegrationTest.java
@@ -12,7 +12,11 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -45,19 +49,82 @@ public class MainIntegrationTest {
 			return false;
 		}
 	}
+	
+   public static boolean vnfmReady()
+   {
+    Connection con = null;
+    Statement st = null;
+    ResultSet rs = null;
+
+    String url = "jdbc:mysql://localhost:3306/openbaton";
+    String user = "root";
+    String password = "root";
+
+    try {
+        con = DriverManager.getConnection(url, user, password);
+        st = con.createStatement();
+        rs = st.executeQuery("select * from vnfm_manager_endpoint");
+        
+        boolean val = rs.next(); //next() returns false if there are no-rows retrieved 
+        if(val==false){
+        	return false;
+         }else
+         {
+        	return true;
+         }
+
+    } catch (SQLException ex) {
+    	log.debug("error db");
+    } finally {
+        try {
+            if (rs != null) {
+                rs.close();
+            }
+            if (st != null) {
+                st.close();
+            }
+            if (con != null) {
+                con.close();
+            }
+
+        } catch (SQLException ex) {
+        	log.debug("error db");
+        }
+    }
+    return false;
+   }
+
 
 	public static void testConfiguration() throws ClassNotFoundException,
 			SQLException, FileNotFoundException, IOException,
 			URISyntaxException, InterruptedException, ExecutionException {
+		
 
-		Runnable r = new Runnable() {
+	  Runnable vnfm = new Runnable() {
 			public void run() {
+			
+			try {
+				Runtime.getRuntime().exec("java -jar /home/tce/neutrino/vnfm/dummy-vnfm/build/libs/dummy-vnfm.jar" );
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				log.error(" VNFN NOT STARTED");
+				e1.printStackTrace();
+			}
+            			
+
+			}
+		};
+				
+		Runnable openbaton = new Runnable() {
+			public void run() {
+				
+
 				log.info("################################# TEST CONFIGURATION #############################################");
 				context = SpringApplication.run(Application.class);
 			}
 		};
 
-		new Thread(r).start();
+		new Thread(openbaton).start();
 
 		while (!available("127.0.0.1", 8080)) {
 			log.debug("waiting the server to start");
@@ -69,9 +136,23 @@ public class MainIntegrationTest {
 			}
 		}
 
-		log.info("STARTED!!!");
+		log.info("OPENBATON STARTED!!!");
 		//log.info("Working Directory = " +
 		//log.info("user.dir"));
+		
+		new Thread(vnfm).start();
+		
+		while (!vnfmReady()) {
+			log.debug("waiting for vnfm registration...");
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		log.info("VNFN STARTED!!!");
 		
         
 		ExecutorService threadpool = Executors.newFixedThreadPool(1);
--- src/main/java/org/project/neutrino/integration/test/NetworkServiceRecord.java
+++ src/main/java/org/project/neutrino/integration/test/NetworkServiceRecord.java
@@ -1,84 +1,105 @@
 package org.project.neutrino.integration.test;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.concurrent.Callable;
+
+import net.minidev.json.parser.JSONParser;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.shell.support.util.FileUtils;
 
 @SuppressWarnings("rawtypes")
+public class NetworkServiceRecord implements Callable {
 
-public class NetworkServiceRecord implements Callable{
-	
 	private Logger log = LoggerFactory.getLogger(this.getClass());
 
 	@Override
 	public Boolean call() throws Exception {
 
 		Boolean output = false;
-        output =  NetworkServiceRecordCreate();
-		
+		output = NetworkServiceRecordCreate();
+
 		return output;
 	}
-	
-	private boolean NetworkServiceRecordCreate() throws URISyntaxException
-	{
-		String body = FileUtils.read(new File("./src/main/resources/vim-instance.json"));
-		 
-		 log.info("SEND REQUEST CREATE VIM");
-		 try (CloseableHttpClient httpClient = HttpClientBuilder.create().build()) {
-			    URI uri = new URI(
-					    "http://127.0.0.1:8080", 
-					    "/api/v1/datacenters",
-					    null);
-	            HttpPost request = new HttpPost(uri);
-	            StringEntity params = new StringEntity(body);
-	            request.addHeader("content-type", "application/json");
-	            request.setEntity(params);
-	            HttpResponse result = httpClient.execute(request);
-	            log.info("RESPONSE VIM RECIVED");
-	     
-	            log.info(result.toString());
 
-	            
-	             } catch (IOException ex) {
-	            	 return false;
-	        }
-		
-		 
-         String body2 = FileUtils.read(new File("./src/main/resources/NetworkServiceDescriptor.json"));
-		 
-		 log.info("SEND REQUEST CREATE NSD");
-		 try (CloseableHttpClient httpClient = HttpClientBuilder.create().build()) {
-			    URI uri = new URI(
-					    "http://127.0.0.1:8080", 
-					    "/api/v1/ns-descriptors",
-					    null);
-	            HttpPost request = new HttpPost(uri);
-	            StringEntity params = new StringEntity(body2);
-	            request.addHeader("content-type", "application/json");
-	            request.setEntity(params);
-	            HttpResponse result = httpClient.execute(request);
-	            log.info("RESPONSE NSD RECIVED");
-	            
-	            log.info(result.toString());
+	private boolean NetworkServiceRecordCreate() throws URISyntaxException {
+		String body = FileUtils.read(new File(
+				"./src/main/resources/vim-instance.json"));
 
-	            
-	             } catch (IOException ex) {
-	            	 return false;
-	        }
-		
+		log.info("SEND REQUEST CREATE VIM");
+		try (CloseableHttpClient httpClient = HttpClientBuilder.create()
+				.build()) {
+			URI uri = new URI("http://127.0.0.1:8080", "/api/v1/datacenters",
+					null);
+			HttpPost request = new HttpPost(uri);
+			StringEntity params = new StringEntity(body);
+			request.addHeader("content-type", "application/json");
+			request.setEntity(params);
+			HttpResponse result = httpClient.execute(request);
+			log.info("RESPONSE VIM RECIVED");
+
+			log.info(result.toString());
+
+		} catch (IOException ex) {
+			return false;
+		}
+
+		String body2 = FileUtils.read(new File(
+				"./src/main/resources/NetworkServiceDescriptor.json"));
+
+		log.info("SEND REQUEST CREATE NSD");
+		try (CloseableHttpClient httpClient = HttpClientBuilder.create()
+				.build()) {
+			URI uri = new URI("http://127.0.0.1:8080",
+					"/api/v1/ns-descriptors", null);
+			HttpPost request = new HttpPost(uri);
+			StringEntity params = new StringEntity(body2);
+			request.addHeader("content-type", "application/json");
+			request.setEntity(params);
+			HttpResponse response = httpClient.execute(request);
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+					response.getEntity().getContent(), "UTF-8"));
+			
+			
+
+		} catch (IOException ex) {
+			return false;
+		}
+
+		/*
+		 * String body3 = FileUtils.read(new
+		 * File("./src/main/resources/NetworkServiceDescriptor.json"));
+		 * 
+		 * log.info("SEND REQUEST CREATE NSR"); try (CloseableHttpClient
+		 * httpClient = HttpClientBuilder.create().build()) { URI uri = new URI(
+		 * "http://127.0.0.1:8080", "/api/v1/ns-records/", null); HttpPost
+		 * request = new HttpPost(uri); StringEntity params = new
+		 * StringEntity(body3); request.addHeader("content-type",
+		 * "application/json"); request.setEntity(params); HttpResponse result =
+		 * httpClient.execute(request); log.info("RESPONSE NSR RECIVED");
+		 * 
+		 * log.info(result.toString());
+		 * 
+		 * 
+		 * } catch (IOException ex) { return false; }
+		 */
+
 		return true;
-		
+
 	}
 
 }
